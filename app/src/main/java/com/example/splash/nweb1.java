eb4864051630880cac9f8afd7c26d1b4802bdee9
Nithin1489 <sainithin3418@gmail.com>
2024-02-27 15:13:18 +0530

    splash screen web

---
+++ app/src/main/java/com/example/splash/nweb1.java
@@ -0,0 +1,14 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nweb1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nweb1);
+    }
+}
